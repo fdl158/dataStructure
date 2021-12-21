1e79baabd726d7b0fa2e485be33e470da575b015
一颗苦笑糖 <3238760835@qq.com>
2021-12-21 13:09:25 +0800

    力扣刷题

---
+++ LeetCode/Stack20.java
@@ -0,0 +1,43 @@
+package cn.delingw.Array.LeetCode;
+
+import java.util.Stack;
+
+/**
+ * @author delingw
+ * @version 1.0
+ * 有效的括号
+ */
+public class Stack20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char topE = stack.pop();
+                if (c == ')' && topE != '(') {
+                    return false;
+                }
+                if (c == '}' && topE != '{') {
+                    return false;
+                }
+                if (c == ']' && topE != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Stack20 stack20 = new Stack20();
+        String s = "()[]{}";
+        String s2 = "(]";
+        System.out.println(stack20.isValid(s));
+        System.out.println(stack20.isValid(s2));
+    }
+}
